a9b173e4a734653ca8f5e190c7a29364fd1b9488
hayeong <hayoungju98@naver.com>
2023-08-30 11:56:16 +0900

    홈페이지 틀

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Nav from './components/Nav/Nav'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import './globals.css'
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
     <html>
       <body >
         <Header />
+        <Nav />
         {children}
         <Footer />
       </body>
